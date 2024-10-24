877fbf3a3b183febd9da6925b5b0af251f2e231d
Christian-2003 <80711177+Christian-2003@users.noreply.github.com>
2024-10-24 13:52:36 +0200

    Implemented new DetailDialog

--- app/src/main/java/de/passwordvault/view/entries/dialog_detail/DetailDialog.java
+++ app/src/main/java/de/passwordvault/view/entries/dialog_detail/DetailDialog.java
@@ -5,7 +5,6 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,36 +31,61 @@ import de.passwordvault.model.detail.Detail;
  */
 public class DetailDialog extends PasswordVaultBottomSheetDialog<DetailViewModel> {
 
+    /**
+     * Class implements a text watcher for the {@link #nameEditText}.
+     */
     private class NameTextWatcher implements TextWatcher {
 
         @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
         }
 
+        /**
+         * Method informs the view model that the text within {@link #nameEditText} has changed.
+         *
+         * @param s         Text of the edit text.
+         * @param start     Index at which the edited text starts.
+         * @param before    Length before the text changed.
+         * @param count     Number of characters that are being changed.
+         */
         @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
             viewModel.setNameEnteredAutomatically(false);
         }
 
         @Override
-        public void afterTextChanged(Editable editable) {
+        public void afterTextChanged(Editable s) {
 
         }
+
     }
 
-
+    /**
+     * Class implements a text watcher for the {@link #typeSelector}.
+     */
     private class TypeTextWatcher implements TextWatcher {
 
         @Override
-        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
         }
 
+
+        /**
+         * Method informs the view model that the text within {@link #typeSelector} has changed.
+         * This changes the text of {@link #nameEditText} and the state of {@link #obfuscateCheckbox}
+         * if their content was not changed manually by the user. Both changes are made without
+         * triggering the {@link #nameTextWatcher} and {@link #obfuscatedCheckedChangeListener}.
+         *
+         * @param s         Text of the edit text.
+         * @param start     Index at which the edited text starts.
+         * @param before    Length before the text changed.
+         * @param count     Number of characters that are being changed.
+         */
         @Override
-        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            Log.d("DetailDialog", "Type changed to " + charSequence);
-            int index = viewModel.getTypeIndexFromName(charSequence.toString());
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            int index = viewModel.getTypeIndexFromName(s.toString());
             if (index == -1) {
                 return;
             }
@@ -91,10 +115,21 @@ public class DetailDialog extends PasswordVaultBottomSheetDialog<DetailViewModel
         public void afterTextChanged(Editable editable) {
 
         }
+
     }
 
+    /**
+     * Class implements a checked change listener for the {@link #obfuscateCheckbox}.
+     */
     private class ObfuscatedCheckedChangeListener implements CompoundButton.OnCheckedChangeListener {
 
+        /**
+         * Method is called whenever the {@link #obfuscateCheckbox} is clicked and informs the view
+         * model that it's state has changed.
+         *
+         * @param checkbox  Checkbox whose state changed.
+         * @param checked   Whether the checkbox is checked or not.
+         */
         @Override
         public void onCheckedChanged(CompoundButton checkbox, boolean checked) {
             viewModel.setObfuscatedEnteredAutomatically(false);
@@ -113,28 +148,67 @@ public class DetailDialog extends PasswordVaultBottomSheetDialog<DetailViewModel
     public static final String ARG_DETAIL = "arg_detail";
 
 
+    /**
+     * Attribute stores the text watcher triggered whenever the name entered changes.
+     */
     private NameTextWatcher nameTextWatcher;
 
+    /**
+     * Attribute stores the text watcher triggered whenever the type selected changes.
+     */
     private TypeTextWatcher typeTextWatcher;
 
+    /**
+     * Attribute stores the checked change listener triggered whenever the state of the obfuscated
+     * checkbox changes.
+     */
     private ObfuscatedCheckedChangeListener obfuscatedCheckedChangeListener;
 
+    /**
+     * Attribute stores the edit text used to enter the name for the detail.
+     */
     private TextInputEditText nameEditText;
 
+    /**
+     * Attribute stores the edit text used to enter the content for the detail.
+     */
     private TextInputEditText contentEditText;
 
+    /**
+     * Attribute stores the selector used to select the type of the detail.
+     */
     private AutoCompleteTextView typeSelector;
 
+    /**
+     * Attribute stores the container for the {@link #nameEditText}.
+     */
     private TextInputLayout nameContainer;
 
+    /**
+     * Attribute stores the container for the {@link #contentEditText}.
+     */
     private TextInputLayout contentContainer;
 
+    /**
+     * Attribute stores the checkbox through which to obfuscate the detail.
+     */
     private CheckboxUiView obfuscateCheckbox;
 
+    /**
+     * Attribute store the checkbox through which to change whether the detail is visible.
+     */
     private CheckboxUiView visibleCheckbox;
 
+    /**
+     * Attribute stores the checkbox through which to change whether to use the detail as username
+     * for autofill.
+     */
     private CheckboxUiView usernameCheckbox;
 
+    /**
+     * Attribute stores the checkbox through which to change whether to use the detail as password
+     * for autofill.
+     */
     private CheckboxUiView passwordCheckbox;
 
 
@@ -151,6 +225,14 @@ public class DetailDialog extends PasswordVaultBottomSheetDialog<DetailViewModel
     }
 
 
+    /**
+     * Method is called whenever the view for the dialog is created.
+     *
+     * @param inflater              Layout inflater to use in order to inflate the dialog view.
+     * @param container             Parent view of the dialog.
+     * @param savedInstanceState    Previously saved state of the instance.
+     * @return                      Created view for the dialog.
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -211,11 +293,31 @@ public class DetailDialog extends PasswordVaultBottomSheetDialog<DetailViewModel
         nameEditText.addTextChangedListener(nameTextWatcher);
         typeSelector.addTextChangedListener(typeTextWatcher);
 
-        Log.d("DetailDialog", "Setup complete");
+        view.findViewById(R.id.button_cancel).setOnClickListener(v -> {
+            if (callback != null) {
+                callback.onCallback(this, Callback.RESULT_CANCEL);
+            }
+            dismiss();
+        });
+
+        view.findViewById(R.id.button_save).setOnClickListener(v -> {
+            if (validateUserInput()) {
+                if (callback != null) {
+                    callback.onCallback(this, Callback.RESULT_SUCCESS);
+                }
+                dismiss();
+            }
+        });
+
         return view;
     }
 
 
+    /**
+     * Method changes the input type for the {@link #contentEditText}.
+     *
+     * @param obfuscate Whether to obfuscate the content of the edit text.
+     */
     private void changeObfuscateContentState(boolean obfuscate) {
         if (obfuscate) {
             contentEditText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
@@ -227,6 +329,43 @@ public class DetailDialog extends PasswordVaultBottomSheetDialog<DetailViewModel
             contentContainer.setEndIconMode(TextInputLayout.END_ICON_NONE);
             contentEditText.setTransformationMethod(null);
         }
+    }
+
+
+    /**
+     * Method copies all user input to the detail within the view model and changes the UI to inform
+     * the user about invalid inputs.
+     *
+     * @return  Whether all input is valid or not.
+     */
+    private boolean validateUserInput() {
+        Detail detail = viewModel.getDetail();
+        if (detail == null) {
+            return false;
+        }
+        detail.setName(nameEditText.getText() != null ? nameEditText.getText().toString() : "");
+        detail.setContent(contentEditText.getText() != null ? contentEditText.getText().toString() : "");
+        detail.setObfuscated(obfuscateCheckbox.isChecked());
+        detail.setVisible(visibleCheckbox.isChecked());
+        detail.setUsername(usernameCheckbox.isChecked());
+        detail.setPassword(passwordCheckbox.isChecked());
+        boolean allInputsValid = true;
+        if (detail.getName().isEmpty()) {
+            allInputsValid = false;
+            nameContainer.setError(getString(R.string.error_empty_input));
+        }
+        else {
+            nameContainer.setErrorEnabled(false);
+        }
+        if (detail.getContent().isEmpty()) {
+            allInputsValid = false;
+            contentContainer.setError(getString(R.string.error_empty_input));
+        }
+        else {
+            contentContainer.setErrorEnabled(false);
+        }
+
+        return allInputsValid;
     }
 
 }
