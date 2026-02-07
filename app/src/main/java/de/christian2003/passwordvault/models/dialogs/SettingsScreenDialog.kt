06d4e6d04b1a80a72df2d0c90630c7713d4a4b03
Christian-2003 <chst0211@gmail.com>
2026-02-07 11:57:56 +0100

    Change project structure

--- app/src/main/java/de/christian2003/passwordvault/models/dialogs/SettingsScreenDialog.kt
+++
@@ -1,9 +0,0 @@
-package de.christian2003.passwordvault.models.dialogs
-
-/**
- * Stores all dialogs that can be displayed on the settings screen.
- */
-enum class SettingsScreenDialog {
-    None,
-    Contrast
-}
